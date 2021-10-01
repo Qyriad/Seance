8318d0c2476afc2006df0dd1fc221ad71903ed62
Alexi Dewfall <git@aksan.dev>
2021-10-01 18:14:08 +0100

    Don't @ me

--- seance/discord_bot.py
+++ seance/discord_bot.py
@@ -253,12 +253,11 @@ class SeanceClient(discord.Client):
         # Copy over any attachments, and copy the inline reply if any.
         files = [await att.to_file() for att in message.attachments]
         ref = message.reference
+        mention_flag = True
         if ref is not None:
             ref.fail_if_not_exists = False
             if message.reference.resolved.author.id not in map(lambda x: x.id, message.mentions):
                 mention_flag = False
-            else:
-                mention_flag = True
 
         # Send the new message.
         await message.channel.send(new_content, files=files, reference=ref, mention_author=mention_flag)
